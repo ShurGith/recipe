f938a3bdade02872f46b1e21e4ef94411e20754b
ShurGith <75266693+ShurGith@users.noreply.github.com>
2025-05-27 11:15:26 +0200

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  /*
+  Simple Omelette Recipe An easy and quick dish, perfect for any meal. 
+  This classic omelette combines beaten eggs cooked to perfection, optionally filled with your choice of cheese, vegetables, or meats. 
+  Preparation time Total: Approximately 10 minutes Preparation: 5 minutes 
+  Cooking: 5 minutes 
+  Ingredients 2-3 large eggs Salt, to taste Pepper, to taste 1 tablespoon of butter or oil Optional fillings: cheese, diced vegetables, cooked meats, herbs 
+  Instructions Beat the eggs: In a bowl, beat the eggs with a pinch of salt and pepper until they are well mixed. You can add a tablespoon of water or milk for a fluffier texture. Heat the pan: Place a non-stick frying pan over medium heat and add butter or oil. Cook the omelette: Once the butter is melted and bubbling, pour in the eggs. 
+  Tilt the pan to ensure the eggs evenly coat the surface. 
+  Add fillings (optional): When the eggs begin to set at the edges but are still slightly runny in the middle, sprinkle your chosen fillings over one half of the omelette. 
+  Fold and serve: As the omelette continues to cook, carefully lift one edge and fold it over the fillings. 
+  Let it cook for another minute, then slide it onto a plate. Enjoy: Serve hot, with additional salt and pepper if needed. 
+  Nutrition The table below shows nutritional values per serving without the additional fillings. 
+  Calories 277kcal Carbs 0g Protein 20g Fat 22g
+*/
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <section className="bg-rose1 w-full h-screen flex justify-center lg:pt-24">
+        <div className=" bg-white w-1/2 margin-auto flex flex-col border" >
+          <img src="/image-omelette.jpeg" className="object-contain" alt="omelette" />
+
+        </div>
+      </section>
+
     </>
   )
 }
